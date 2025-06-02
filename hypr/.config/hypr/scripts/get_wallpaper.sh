c29bbaf67257a19ab21aae26b2441a86f9b6ce72
j-kling95 <julian.kling@mail-me.net>
2025-06-02 18:40:42 +0200

    Further cleanup of files, simplified wallpapermenu script

--- hypr/.config/hypr/scripts/get_wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-current_wallpaper="$(cat $HOME/.cache/wal/wal)"
-
-echo "\$new_wallpaper=$current_wallpaper" > ~/.config/hypr/scripts/wallpaper
-
-exit
---
+++ hypr/.config/hypr/scripts/wallpapermenu.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+FOLDER=~/wallpaper
+
+menu () {
+  WALLPAPER="$(for a in $HOME/wallpaper/*; do echo -en "$a\0icon\x1f$a\n" ; done | rofi -dmenu -config ~/dotfiles/rofi/.config/rofi/wallpaper.rasi)"
+
+
+  case "$WALLPAPER" in
+    *.*)
+      wal -s --cols16 lighten -i $WALLPAPER && refresh
+      ;;
+    *)
+      exit 0
+      ;;
+  esac
+
+#  read -p "1=Dark Theme, 2=Light Theme: \n" theme 
+#  echo $theme
+#  
+#  case "$theme" in
+#    1)
+#      wal -s --cols16 lighten -i $CHOICE && refresh
+#      ;;
+#    2)
+#      wal -s -l --cols16 lighten -i $CHOICE && refresh
+#      ;;
+#  esac
+
+}
+
+refresh () {
+  swww img $WALLPAPER;
+  pkill waybar; source $HOME/.config/waybar/venv/bin/activate && hyprctl dispatch exec waybar;
+  bash $HOME/.config/dunst/pywal.sh;
+#  echo "\$new_wallpaper=$WALLPAPER" > ~/.config/hypr/scripts/wallpaper
+  sed -i "1c\$new_wallpaper=$WALLPAPER" ~/.config/hypr/hyprlock.conf 
+}
+
+menu
+kitty @ close-window
